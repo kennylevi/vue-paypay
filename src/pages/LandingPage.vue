bdebe140cfa153f2e430ea9e2a87206511a478ad
kenny levi <kennylevi@Kennys-MacBook-Pro.local>
2023-01-23 10:09:22 +0100

    i did npm install for the project packages and remove unused filed/folders for clean start, i also installed sass

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <main>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    A accusantium aliquid consequatur, doloremque dolores eius harum ipsa ipsum magni maiores neque nihil, officiis placeat quasi qui quidem repellat voluptate voluptates.
+  </main>
+</template>
+
+<style lang="sass">
+
+</style>
